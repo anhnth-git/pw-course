f4c2930721095d51d412aa5eb02b36b51525f867
anhnth <anhtnhb@etc.vn>
2026-03-04 21:18:25 +0700

    lession 2 project

---
+++ test/lession-02/ex3.js
@@ -0,0 +1,10 @@
+let a= true;
+let b= false;
+let c= true;
+
+console.log(a&&b&&C); 
+// la false vi ca 3 cai la true moi duoc
+console.log(a&&b||c);
+// do a la true thi chi can 1 trong 2 b hoac c la true la duoc
+
+
