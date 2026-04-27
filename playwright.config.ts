80e04d63a1c8bca506ce268b4b72c436d4e23f6d
anhnth <anhtnhb@etc.vn>
2026-04-27 21:05:39 +0700

    ignore api-hrm test in CI/CD

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  /* Ignore internal API tests that can't run on CI */
+  testIgnore: ['**/api-hrm.spec.ts'],
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
